5cbde4ae5cb5bc537eab91c0be8dd71aea289689
Gaurav <gauravpbulbule@gmail.com>
2026-03-06 00:03:30 +0530

    Added Program to Print Hellow Square Star Pattern

--- src/main/java/pattern_programs/hellowSquarePattern.java
+++ src/main/java/pattern_programs/hellowSquarePattern.java
@@ -18,7 +18,7 @@ public class hellowSquarePattern {
     private static void printHellowSquarePattern(int n){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++) {
-                if(i==1||i==n||j==1||j==n) {
+                if(i==1||i==n||j==1||j==n){
                     System.out.print("* ");
                 }
                 else{
