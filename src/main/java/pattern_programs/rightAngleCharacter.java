421270cc8bb833da1269fa6eb3e229a6a0a0685a
Gaurav <gauravpbulbule@gmail.com>
2026-03-06 22:29:56 +0530

    Added Program to Print Right Angle Character Pattern

--- src/main/java/pattern_programs/rightAngleCharacter.java
+++ src/main/java/pattern_programs/rightAngleCharacter.java
@@ -4,7 +4,7 @@ package pattern_programs;
 import java.util.Scanner;
 
 public class rightAngleCharacter {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Number.");
         int n = sc.nextInt();
