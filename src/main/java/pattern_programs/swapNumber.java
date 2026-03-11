9a2d2a7fd51a1eabccebecaeb444b7efd8948093
Gaurav <gauravpbulbule@gmail.com>
2026-03-11 23:16:31 +0530

    Added program to swap two numbers without using a temporary variable.

---
+++ src/main/java/pattern_programs/swapNumber.java
@@ -0,0 +1,12 @@
+package pattern_programs;
+public class swapNumber {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        System.out.println("Before Swap: a = " + a + ", b = " + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After Swap: a = " + a + ", b = " + b);
+    }
+}
+
